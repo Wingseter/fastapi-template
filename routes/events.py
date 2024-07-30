a7d126c18a2972e1b58a6224033f69666d1503f1
kwon <wingseter@gmail.com>
2024-07-30 19:56:56 +0900

    event search list router

--- routes/events.py
+++ routes/events.py
@@ -1,19 +1,36 @@
-from pydantic import BaseModel
+from fastapi import APIRouter, Body, HTTPException, status
+from models.events import Event
 from typing import List
 
-class Event(BaseModel):
-    id: int
-    title: str
-    image: str
-    description: str
-    tags: List[str]
-    location: str
+event_router = APIRouter(
+    tags=["Events"]
+)
 
-    class Config:
-        scheme_extra = {
-            "title": "FastAPI Book Launch",
-            "image": "https://linktomyimage.com/image.png",
-            "description": "Wesill be discussing the contents of the FastAPI",
-            "tags":["python", "fastapi", "book"],
-            "location": "Gooogle Meet"
-        }
+events = []
+
+@event_router.get("/", response_model = List[Event])
+async def retrive_all_evnets() -> List[Event]:
+    for event in events:
+        if event.id == id:
+            return event
+    raise HTTPException(
+        status_code = status.HTTP_404_NOT_FOUND,
+        detail="Event with supplied ID does not exist"
+    )
+
+@event_router.post("/new")
+async def create_event(body:Event = Body(...)) -> dict:
+    events.append(body)
+    return {
+        "message": "Event created successfully"
+    }
+
+@event_router.delete("/{id}")
+async def delete_event(id: int) ->dict:
+    for event in events:
+        if event.id == id:
+            return event
+    raise HTTPException(
+        status_code = status.HTTP_404_NOT_FOUND,
+        detail="Event with supplied ID does not exist"
+    )
