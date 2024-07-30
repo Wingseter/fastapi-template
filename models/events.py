38eee23a5392081e4c1a8fa0aa7bc3d73a35a22c
kwon <wingseter@gmail.com>
2024-07-30 22:28:27 +0900

    event control with database

--- models/events.py
+++ models/events.py
@@ -1,19 +1,40 @@
-from pydantic import BaseModel
-from typing import List
+from sqlmodel import JSON, SQLModel, Field, Column
+from typing import Optional, List
 
-class Event(BaseModel):
-    id: int
+class Event(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
     title: str
     image: str
     description: str
-    tags: List[str]
+    tags: List[str] = Field(sa_column = Column(JSON))
     location: str
 
     class Config:
+        arbitary_types_allowed = True
         scheme_extra = {
-            "title": "FastAPI Book Launch",
-            "image": "https://linktomyimage.com/image.png",
-            "description": "Wesill be discussing the contents of the FastAPI",
-            "tags":["python", "fastapi", "book"],
-            "location": "Gooogle Meet"
+            "example" : {
+                "title" : "FastAPI",
+                "image" : "https://linktoimage.com/image.png",
+                "description" : "FastAPi Config",
+                "tags" : ["python", "fastapi", "book", "launch"],
+                "location" : "Google Meet" 
+            }
+        }
+
+class EventUpdate(SQLModel):
+    title: Optional[str]
+    image: Optional[str]
+    description: Optional[str]
+    tags: Optional[List[str]]
+    location: Optional[str]
+
+    class Config:
+        schema_extra = {
+            "example" : {
+                "title": "FastAPI",
+                "image": "https://linktoimage.com/image.png",
+                "description": "FastAPI Config",
+                "tags" : ["python", "fastapi", "book", "launch"],
+                "location": "Google Meet"
+            }
         }
