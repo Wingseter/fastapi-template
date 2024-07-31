3388ea0d85225dcf119061fb64c135c90d56afa6
kwon <wingseter@gmail.com>
2024-07-31 13:37:39 +0900

    sqlite setting

--- routes/events.py
+++ routes/events.py
@@ -37,8 +37,9 @@ async def retrieve_event(id:int, session=Depends(get_session)) -> Event:
         detail="Event with supplied ID does not exist"
     )
 
-@event_router.delete("/{id}")
-async def delete_event(id: int) ->dict:
+@event_router.delete("/delete/{id}")
+async def delete_event(id: int, session=Depends(get_session)) ->dict:
+    event = session.get(Event, id)
     for event in events:
         if event.id == id:
             return event
@@ -53,3 +54,20 @@ async def delete_all_events() -> dict:
     return {
         "message" : "Event deleted successfully"
     }
+
+@event_router.put("/edit/{id}", response_model = Event)
+async def update_event(id:int, new_data:EventUpdate, session=Depends(get_session)) ->Event:
+    event = session.get(Event, id)
+    if event:
+        event_data = new_data.dict(exclude_unset=True)
+        for key, value in event_data.items():
+            setattr(event, key, value)
+        session.add(event)
+        session.commit()
+        session.refresh(event)
+
+        return event
+    raise HTTPException(
+        status_code = status.HTTP_404_NOT_FOUND,
+        detail="Event with supplied ID does not exist"
+    )
